ea556d20ee2199928974e8d3eb5b2e50f37b2f41
abhijeetsenguptaa <101327881+abhijeetsenguptaa@users.noreply.github.com>
2023-06-06 11:36:14 +0530

    basic structure and the server has been connected to the database

---
+++ server.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const { connection } = require('./configs/connection');
+
+
+
+const PORT = process.env.port || 8080;
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+
+app.get('/',async(req,res)=>{
+    res.status(200).send({
+        status : true,
+        msg : 'Welcome to the Social-Media-App.'
+    })
+})
+
+
+
+
+
+app.listen(PORT,async()=>{
+    try{
+        await connection;
+        console.log('Server is connected to the Database.');
+    }catch{
+        console.log('Server could not connect with the server.');
+    }
+    console.log(`Server is running at the port ${PORT}`);
+})
